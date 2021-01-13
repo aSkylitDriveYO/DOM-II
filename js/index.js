bec97022ad6bc1502150f4f71e18660f522e6500
aSkylitDriveYO <cochrisortega@gmail.com>
2021-01-12 18:01:54 -0700

    scroll

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // Your code goes here
+
+
+const imageFunBus = document.getElementById("funBus");
+const SignUpButton = document.querySelectorAll (".btn");
+const heading = document.getElementsByTagName("header");
+
+/*
+imageFunBus.onmouseover = function(){
+    imageFunBus.style.display='none';
+}
+*/
+
+SignUpButton.forEach(item=> {
+    item.addEventListener('click', () => {
+        prompt("Your Name?");
+    })
+});
+
+window.addEventListener("scroll", () => {
+    const max = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+
+    if (Math.ceil(scrolled) === 1301) {
+        alert('you have reached the bottom');
+    }
+    console.log(scrolled);
+    console.log(max);
+    
+});
+
+
+
+
+/*buttonFunBus.onclick = function(){
+    imageFunBus.style.display='none';
+} */
+
+
